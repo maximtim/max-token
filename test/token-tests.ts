eab2ba6de33e04c3b0091446fd9d046746b73ca4
timofeev <maximtim1337@gmail.com>
2022-04-15 22:20:53 +0300

    refactor

--- test/token-tests.ts
+++ test/token-tests.ts
@@ -16,16 +16,6 @@ async function getBalance(maxToken: Contract, signer: SignerWithAddress) {
   return await maxToken.balanceOf(await signer.getAddress());
 }
 
-function getGasCost(txRes: any) {
-  return txRes.gasUsed.mul(txRes.effectiveGasPrice);
-}
-
-function delay(n : number){
-  return new Promise(function(resolve){
-      setTimeout(resolve,n*1000);
-  });
-}
-
 ///////////////////////////////////////////////////////////////////////////////
 
 describe("MaxToken", function () {
@@ -33,6 +23,10 @@ describe("MaxToken", function () {
      first: SignerWithAddress, 
      second: SignerWithAddress, 
      third : SignerWithAddress;
+  let ownerAddr : string,
+     firstAddr: string, 
+     secondAddr: string, 
+     thirdAddr : string;
   let maxToken : Contract;
   const supply = 1000_000;
   const name = "MaxToken";
@@ -41,6 +35,10 @@ describe("MaxToken", function () {
 
   beforeEach(async () => {
     [ owner, first, second, third ] = await ethers.getSigners();
+    ownerAddr = await owner.getAddress();
+    firstAddr = await first.getAddress();
+    secondAddr = await second.getAddress();
+    thirdAddr = await third.getAddress();
     const MaxToken = await hre.ethers.getContractFactory("MaxToken");
     maxToken = await MaxToken.deploy(supply, name, symbol, decimals);
     await maxToken.deployed();
@@ -49,34 +47,30 @@ describe("MaxToken", function () {
   it("should deploy successfully", async function () {
     expect(maxToken.address).to.be.properAddress;
     expect(await getBalance(maxToken, owner)).to.equal(supply);
-    expect(await maxToken.minters(await owner.getAddress())).to.be.true;
+    expect(await maxToken.minters(ownerAddr)).to.be.true;
   });
 
   it("Should transfer successfully", async function () {
-    const decimals = await maxToken.decimals();
-    const symbol = await maxToken.symbol();
-    const transferValue = parseUnits("10", await maxToken.decimals());
+    const transferValue = parseUnits("10", decimals);
 
     expect(await getBalance(maxToken, owner)).to.equal(supply);
     expect(await getBalance(maxToken, first)).to.equal(0);
 
-    await expect(maxToken.connect(owner).transfer(await first.getAddress(), transferValue))
+    await expect(maxToken.connect(owner).transfer(firstAddr, transferValue))
       .to.emit(maxToken, "Transfer")
-      .withArgs(await owner.getAddress(), await first.getAddress(), transferValue);
+      .withArgs(ownerAddr, firstAddr, transferValue);
 
     expect(await getBalance(maxToken, owner)).to.equal(supply - transferValue.toNumber());
     expect(await getBalance(maxToken, first)).to.equal(transferValue);
   });
 
   it("Should fail transfer when insufficient funds", async function () {
-    const decimals = await maxToken.decimals();
-    const symbol = await maxToken.symbol();
-    const transferValue = parseUnits("10000", await maxToken.decimals());
+    const transferValue = parseUnits("10000", decimals);
 
     expect(await getBalance(maxToken, owner)).to.equal(supply);
     expect(await getBalance(maxToken, first)).to.equal(0);
 
-    await expect(maxToken.connect(owner).transfer(await first.getAddress(), transferValue))
+    await expect(maxToken.connect(owner).transfer(firstAddr, transferValue))
         .to.be.revertedWith('Not enough tokens on balance');
 
     expect(await getBalance(maxToken, owner)).to.equal(supply);
@@ -84,40 +78,36 @@ describe("MaxToken", function () {
   });
 
   it("Should approve successfully", async function () {
-    const decimals = await maxToken.decimals();
-    const symbol = await maxToken.symbol();
-    const approveValue = parseUnits("10", await maxToken.decimals());
+    const approveValue = parseUnits("10", decimals);
 
     expect(await getAllowance(maxToken, owner, first)).to.equal(0);
 
-    await expect(maxToken.connect(owner).approve(await first.getAddress(), approveValue))
+    await expect(maxToken.connect(owner).approve(firstAddr, approveValue))
       .to.emit(maxToken, "Approval")
-      .withArgs(await owner.getAddress(), await first.getAddress(), approveValue);
+      .withArgs(ownerAddr, firstAddr, approveValue);
 
     expect(await getAllowance(maxToken, owner, first)).to.equal(approveValue);
   });
 
   it("Should transferFrom successfully if allowed before", async function () {
-    const decimals = await maxToken.decimals();
-    const symbol = await maxToken.symbol();
 
-    const approveValue = parseUnits("100", await maxToken.decimals());
-    const transferValue = parseUnits("10", await maxToken.decimals());
+    const approveValue = parseUnits("100", decimals);
+    const transferValue = parseUnits("10", decimals);
 
     expect(await getAllowance(maxToken, owner, first)).to.equal(0);
 
-    await expect(maxToken.connect(owner).approve(await first.getAddress(), approveValue))
+    await expect(maxToken.connect(owner).approve(firstAddr, approveValue))
       .to.emit(maxToken, "Approval")
-      .withArgs(await owner.getAddress(), await first.getAddress(), approveValue);
+      .withArgs(ownerAddr, firstAddr, approveValue);
 
     expect(await getAllowance(maxToken, owner, first)).to.equal(approveValue);
 
     expect(await getBalance(maxToken, owner)).to.equal(supply);
     expect(await getBalance(maxToken, second)).to.equal(0);
 
-    await expect(maxToken.connect(first).transferFrom(await owner.getAddress(), await second.getAddress(), transferValue))
+    await expect(maxToken.connect(first).transferFrom(ownerAddr, secondAddr, transferValue))
       .to.emit(maxToken, "Transfer")
-      .withArgs(await owner.getAddress(), await second.getAddress(), transferValue);
+      .withArgs(ownerAddr, secondAddr, transferValue);
 
     expect(await getBalance(maxToken, owner)).to.equal(supply - transferValue.toNumber());
     expect(await getBalance(maxToken, second)).to.equal(transferValue);
@@ -125,24 +115,22 @@ describe("MaxToken", function () {
   });
 
   it("Should fail transferFrom if not enough allowed to spend", async function () {
-    const decimals = await maxToken.decimals();
-    const symbol = await maxToken.symbol();
 
-    const approveValue = parseUnits("5", await maxToken.decimals());
-    const transferValue = parseUnits("10", await maxToken.decimals());
+    const approveValue = parseUnits("5", decimals);
+    const transferValue = parseUnits("10", decimals);
 
     expect(await getAllowance(maxToken, owner, first)).to.equal(0);
 
-    await expect(maxToken.connect(owner).approve(await first.getAddress(), approveValue))
+    await expect(maxToken.connect(owner).approve(firstAddr, approveValue))
       .to.emit(maxToken, "Approval")
-      .withArgs(await owner.getAddress(), await first.getAddress(), approveValue);
+      .withArgs(ownerAddr, firstAddr, approveValue);
 
     expect(await getAllowance(maxToken, owner, first)).to.equal(approveValue);
 
     expect(await getBalance(maxToken, owner)).to.equal(supply);
     expect(await getBalance(maxToken, second)).to.equal(0);
 
-    await expect(maxToken.connect(first).transferFrom(await owner.getAddress(), await second.getAddress(), transferValue))
+    await expect(maxToken.connect(first).transferFrom(ownerAddr, secondAddr, transferValue))
         .to.be.revertedWith('Not enough allowance to spend');
 
     expect(await getBalance(maxToken, owner)).to.equal(supply);
@@ -151,24 +139,22 @@ describe("MaxToken", function () {
   });
 
   it("Should fail transferFrom if insufficient funds", async function () {
-    const decimals = await maxToken.decimals();
-    const symbol = await maxToken.symbol();
 
-    const approveValue = parseUnits("100000", await maxToken.decimals());
-    const transferValue = parseUnits("10000", await maxToken.decimals());
+    const approveValue = parseUnits("100000", decimals);
+    const transferValue = parseUnits("10000", decimals);
 
     expect(await getAllowance(maxToken, owner, first)).to.equal(0);
 
-    await expect(maxToken.connect(owner).approve(await first.getAddress(), approveValue))
+    await expect(maxToken.connect(owner).approve(firstAddr, approveValue))
       .to.emit(maxToken, "Approval")
-      .withArgs(await owner.getAddress(), await first.getAddress(), approveValue);
+      .withArgs(ownerAddr, firstAddr, approveValue);
 
     expect(await getAllowance(maxToken, owner, first)).to.equal(approveValue);
 
     expect(await getBalance(maxToken, owner)).to.equal(supply);
     expect(await getBalance(maxToken, second)).to.equal(0);
 
-    await expect(maxToken.connect(first).transferFrom(await owner.getAddress(), await second.getAddress(), transferValue))
+    await expect(maxToken.connect(first).transferFrom(ownerAddr, secondAddr, transferValue))
         .to.be.revertedWith('Not enough tokens on spender balance');
 
     expect(await getBalance(maxToken, owner)).to.equal(supply);
@@ -177,57 +163,49 @@ describe("MaxToken", function () {
   });
 
   it("Should add and remove minter successfully", async function () {
-    const decimals = await maxToken.decimals();
-    const symbol = await maxToken.symbol();
 
-    expect(await maxToken.minters(await first.getAddress())).to.be.false;
-    await expect(maxToken.connect(owner).setMinterRole(await first.getAddress(), true)).to.not.be.reverted;
-    expect(await maxToken.minters(await first.getAddress())).to.be.true;
-    await expect(maxToken.connect(owner).setMinterRole(await first.getAddress(), false)).to.not.be.reverted;
-    expect(await maxToken.minters(await first.getAddress())).to.be.false;
+    expect(await maxToken.minters(firstAddr)).to.be.false;
+    await expect(maxToken.connect(owner).setMinterRole(firstAddr, true)).to.not.be.reverted;
+    expect(await maxToken.minters(firstAddr)).to.be.true;
+    await expect(maxToken.connect(owner).setMinterRole(firstAddr, false)).to.not.be.reverted;
+    expect(await maxToken.minters(firstAddr)).to.be.false;
   });
 
   it("Should fail to set minter if caller is not owner", async function () {
-    const decimals = await maxToken.decimals();
-    const symbol = await maxToken.symbol();
 
-    expect(await maxToken.minters(await first.getAddress())).to.be.false;
-    await expect(maxToken.connect(first).setMinterRole(await first.getAddress(), true)).to.be.revertedWith('Only owner can set minter role');
+    expect(await maxToken.minters(firstAddr)).to.be.false;
+    await expect(maxToken.connect(first).setMinterRole(firstAddr, true)).to.be.revertedWith('Only owner can set minter role');
   });
 
   it("Should mint successfully", async function () {
-    const decimals = await maxToken.decimals();
-    const symbol = await maxToken.symbol();
 
-    const mintValue = parseUnits("100", await maxToken.decimals());
+    const mintValue = parseUnits("100", decimals);
 
-    expect(await maxToken.minters(await first.getAddress())).to.be.false;
-    await expect(maxToken.connect(owner).setMinterRole(await first.getAddress(), true)).to.not.be.reverted;
-    expect(await maxToken.minters(await first.getAddress())).to.be.true;
+    expect(await maxToken.minters(firstAddr)).to.be.false;
+    await expect(maxToken.connect(owner).setMinterRole(firstAddr, true)).to.not.be.reverted;
+    expect(await maxToken.minters(firstAddr)).to.be.true;
 
     expect(await getBalance(maxToken, second)).to.equal(0);
     expect(await maxToken.totalSupply()).to.equal(supply);
 
-    await expect(maxToken.connect(first).mint(await second.getAddress(), mintValue))
+    await expect(maxToken.connect(first).mint(secondAddr, mintValue))
       .to.emit(maxToken, "Transfer")
-      .withArgs(constants.AddressZero, await second.getAddress(), mintValue);
+      .withArgs(constants.AddressZero, secondAddr, mintValue);
 
     expect(await getBalance(maxToken, second)).to.equal(mintValue);
     expect(await maxToken.totalSupply()).to.equal(supply + mintValue.toNumber());
   });
 
   it("Should fail minting if sender is not minter", async function () {
-    const decimals = await maxToken.decimals();
-    const symbol = await maxToken.symbol();
 
-    const mintValue = parseUnits("100", await maxToken.decimals());
+    const mintValue = parseUnits("100", decimals);
 
-    expect(await maxToken.minters(await first.getAddress())).to.be.false;
+    expect(await maxToken.minters(firstAddr)).to.be.false;
 
     expect(await getBalance(maxToken, second)).to.equal(0);
     expect(await maxToken.totalSupply()).to.equal(supply);
 
-    await expect(maxToken.connect(first).mint(await second.getAddress(), mintValue))
+    await expect(maxToken.connect(first).mint(secondAddr, mintValue))
         .to.be.revertedWith('Only minter can mint tokens');
 
     expect(await getBalance(maxToken, second)).to.equal(0);
@@ -235,25 +213,21 @@ describe("MaxToken", function () {
   });
 
   it("Should burn successfully", async function () {
-    const decimals = await maxToken.decimals();
-    const symbol = await maxToken.symbol();
-    const burnValue = parseUnits("10", await maxToken.decimals());
+    const burnValue = parseUnits("10", decimals);
 
     expect(await getBalance(maxToken, owner)).to.equal(supply);
     expect(await maxToken.totalSupply()).to.equal(supply);
 
     await expect(maxToken.connect(owner).burn(burnValue))
       .to.emit(maxToken, "Transfer")
-      .withArgs(await owner.getAddress(), constants.AddressZero, burnValue);
+      .withArgs(ownerAddr, constants.AddressZero, burnValue);
 
     expect(await getBalance(maxToken, owner)).to.equal(supply - burnValue.toNumber());
     expect(await maxToken.totalSupply()).to.equal(supply - burnValue.toNumber());
   });
 
   it("Should fail burn if insufficient funds", async function () {
-    const decimals = await maxToken.decimals();
-    const symbol = await maxToken.symbol();
-    const burnValue = parseUnits("10000", await maxToken.decimals());
+    const burnValue = parseUnits("10000", decimals);
 
     expect(await getBalance(maxToken, owner)).to.equal(supply);
     expect(await maxToken.totalSupply()).to.equal(supply);
@@ -266,66 +240,60 @@ describe("MaxToken", function () {
   });
 
   it("Should transfer successfully", async function () {
-    const decimals = await maxToken.decimals();
-    const symbol = await maxToken.symbol();
-    const transferValue = parseUnits("10", await maxToken.decimals());
+    const transferValue = parseUnits("10", decimals);
 
     expect(await getBalance(maxToken, owner)).to.equal(supply);
     expect(await getBalance(maxToken, first)).to.equal(0);
 
-    await expect(maxToken.connect(owner).transfer(await first.getAddress(), transferValue))
+    await expect(maxToken.connect(owner).transfer(firstAddr, transferValue))
       .to.emit(maxToken, "Transfer")
-      .withArgs(await owner.getAddress(), await first.getAddress(), transferValue);
+      .withArgs(ownerAddr, firstAddr, transferValue);
 
     expect(await getBalance(maxToken, owner)).to.equal(supply - transferValue.toNumber());
     expect(await getBalance(maxToken, first)).to.equal(transferValue);
   });
 
   it("Should burnFrom successfully if allowed before", async function () {
-    const decimals = await maxToken.decimals();
-    const symbol = await maxToken.symbol();
 
-    const approveValue = parseUnits("100", await maxToken.decimals());
-    const burnValue = parseUnits("10", await maxToken.decimals());
+    const approveValue = parseUnits("100", decimals);
+    const burnValue = parseUnits("10", decimals);
 
     expect(await getAllowance(maxToken, owner, first)).to.equal(0);
 
-    await expect(maxToken.connect(owner).approve(await first.getAddress(), approveValue))
+    await expect(maxToken.connect(owner).approve(firstAddr, approveValue))
       .to.emit(maxToken, "Approval")
-      .withArgs(await owner.getAddress(), await first.getAddress(), approveValue);
+      .withArgs(ownerAddr, firstAddr, approveValue);
 
     expect(await getAllowance(maxToken, owner, first)).to.equal(approveValue);
 
     expect(await getBalance(maxToken, owner)).to.equal(supply);
     expect(await maxToken.totalSupply()).to.equal(supply);
 
-    await expect(maxToken.connect(first).burnFrom(await owner.getAddress(), burnValue))
+    await expect(maxToken.connect(first).burnFrom(ownerAddr, burnValue))
       .to.emit(maxToken, "Transfer")
-      .withArgs(await owner.getAddress(), constants.AddressZero, burnValue);
+      .withArgs(ownerAddr, constants.AddressZero, burnValue);
 
     expect(await getBalance(maxToken, owner)).to.equal(supply - burnValue.toNumber());
     expect(await maxToken.totalSupply()).to.equal(supply - burnValue.toNumber());
   });
 
   it("Should fail burnFrom if not enough allowance", async function () {
-    const decimals = await maxToken.decimals();
-    const symbol = await maxToken.symbol();
 
-    const approveValue = parseUnits("10", await maxToken.decimals());
-    const burnValue = parseUnits("100", await maxToken.decimals());
+    const approveValue = parseUnits("10", decimals);
+    const burnValue = parseUnits("100", decimals);
 
     expect(await getAllowance(maxToken, owner, first)).to.equal(0);
 
-    await expect(maxToken.connect(owner).approve(await first.getAddress(), approveValue))
+    await expect(maxToken.connect(owner).approve(firstAddr, approveValue))
       .to.emit(maxToken, "Approval")
-      .withArgs(await owner.getAddress(), await first.getAddress(), approveValue);
+      .withArgs(ownerAddr, firstAddr, approveValue);
 
     expect(await getAllowance(maxToken, owner, first)).to.equal(approveValue);
 
     expect(await getBalance(maxToken, owner)).to.equal(supply);
     expect(await maxToken.totalSupply()).to.equal(supply);
 
-    await expect(maxToken.connect(first).burnFrom(await owner.getAddress(), burnValue))
+    await expect(maxToken.connect(first).burnFrom(ownerAddr, burnValue))
         .to.be.revertedWith('Not enough allowance to burn');
 
     expect(await getBalance(maxToken, owner)).to.equal(supply);
